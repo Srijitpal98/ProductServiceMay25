1bf4b582433da760c6aa66a929516de1787d671b
Srijitpal98 <srijitpal1998@gmail.com>
2025-07-26 13:48:36 +0530

    DB Changes

--- src/main/java/com/ecommerce/productservicemay25/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/productservicemay25/repositories/ProductRepository.java
@@ -1,8 +1,51 @@
 package com.ecommerce.productservicemay25.repositories;
 
+import com.ecommerce.productservicemay25.models.Category;
+import com.ecommerce.productservicemay25.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public class ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    //select * from Product where id = ?
+    @Override
+    Optional<Product> findById(Long productId);
+
+    //select * from products where lower(title) LIKE %iphone%
+    List<Product> findByTitleContainsIgnoreCase(String title);
+
+    //find all the products where price >= 100 and <= 1000
+    List<Product> findByPriceBetween(Double priceAfter, Double priceBefore);
+
+    //search a product of a particular category
+    //select * from products where category_id = category.id;
+    List<Product> findByCategory(Category category);
+
+    List<Product> findAllByCategory_Id(Long categoryId);
+
+    /*
+    Select * from Products P
+    JOIN Category C
+    on p.category_id = c.category_id
+    where c.category_title like 'categoryTitle';
+     */
+    List<Product> findAllByCategory_Title(String categoryTitle);
+
+//    @Query("select title from products where id = ?")
+//    Optional<Product> findOnlyProductTitleById(Long productId);
+
 
 }
+
+
+//public interface UserRepository extends JpaRepository<User, Long> {
+//
+//    List<User> findByLastname(String lastname);
+//
+//    User findByEmailAddress(String emailAddress);
+//}
--- src/main/java/com/ecommerce/productservicemay25/services/SelfProductService.java
+++ src/main/java/com/ecommerce/productservicemay25/services/SelfProductService.java
@@ -1,6 +1,7 @@
 package com.ecommerce.productservicemay25.services;
 
 import com.ecommerce.productservicemay25.models.Product;
+import com.ecommerce.productservicemay25.repositories.ProductRepository;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,12 @@ import java.util.List;
 @Service("selfProductService")
 //@Primary
 public class SelfProductService implements ProductService {
+
+    private ProductRepository productRepository;
+
+    public SelfProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @Override
     public Product getSingleProduct(Long productId) {
